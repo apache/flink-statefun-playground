dce2fa7db176039a43612f966a3536a8a72edc32
Tzu-Li (Gordon) Tai <tzulitai@apache.org>
2021-03-19 13:18:50 +0800

    [FLINK-21865] Add a Docker Compose greeter example
    This closes #4.

---
+++ java/greeter/src/main/java/org/apache/flink/statefun/playground/java/greeter/UserFn.java
@@ -0,0 +1,84 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.statefun.playground.java.greeter;
+
+import static org.apache.flink.statefun.playground.java.greeter.types.Types.USER_LOGIN_JSON_TYPE;
+import static org.apache.flink.statefun.playground.java.greeter.types.Types.USER_PROFILE_PROTOBUF_TYPE;
+
+import java.util.concurrent.CompletableFuture;
+import org.apache.flink.statefun.playground.java.greeter.types.UserLogin;
+import org.apache.flink.statefun.playground.java.greeter.types.generated.UserProfile;
+import org.apache.flink.statefun.sdk.java.Context;
+import org.apache.flink.statefun.sdk.java.StatefulFunction;
+import org.apache.flink.statefun.sdk.java.StatefulFunctionSpec;
+import org.apache.flink.statefun.sdk.java.TypeName;
+import org.apache.flink.statefun.sdk.java.ValueSpec;
+import org.apache.flink.statefun.sdk.java.message.Message;
+import org.apache.flink.statefun.sdk.java.message.MessageBuilder;
+
+/**
+ * A stateful function that is invoked with {@link UserLogin} events, and persists in state storage
+ * the number of times we've seen the user log in as well as the user's last seen timestamp. This
+ * function in-turn invokes {@link GreetingsFn} with the user information to generate a personalized
+ * greetings message.
+ */
+final class UserFn implements StatefulFunction {
+
+  private static final ValueSpec<Integer> SEEN_COUNT = ValueSpec.named("seen_count").withIntType();
+  private static final ValueSpec<Long> SEEN_TIMESTAMP_MS =
+      ValueSpec.named("seen_timestamp_ms").withLongType();
+
+  static final TypeName TYPENAME = TypeName.typeNameOf("greeter.fns", "user");
+  static final StatefulFunctionSpec SPEC =
+      StatefulFunctionSpec.builder(TYPENAME)
+          .withValueSpecs(SEEN_COUNT, SEEN_TIMESTAMP_MS)
+          .withSupplier(UserFn::new)
+          .build();
+
+  @Override
+  public CompletableFuture<Void> apply(Context context, Message message) {
+    if (message.is(USER_LOGIN_JSON_TYPE)) {
+      final UserLogin login = message.as(USER_LOGIN_JSON_TYPE);
+
+      int seenCount = context.storage().get(SEEN_COUNT).orElse(0);
+      seenCount++;
+
+      final long nowMs = System.currentTimeMillis();
+      final long lastSeenTimestampMs = context.storage().get(SEEN_TIMESTAMP_MS).orElse(nowMs);
+
+      context.storage().set(SEEN_COUNT, seenCount);
+      context.storage().set(SEEN_TIMESTAMP_MS, nowMs);
+
+      final UserProfile profile =
+          UserProfile.newBuilder()
+              .setName(login.getUserName())
+              .setLoginLocation(login.getLoginType().name())
+              .setSeenCount(seenCount)
+              .setLastSeenDeltaMs(nowMs - lastSeenTimestampMs)
+              .build();
+      context.send(
+          MessageBuilder.forAddress(GreetingsFn.TYPENAME, login.getUserId())
+              .withCustomType(USER_PROFILE_PROTOBUF_TYPE, profile)
+              .build());
+    } else {
+      throw new IllegalArgumentException("Unexpected message type: " + message.valueTypeName());
+    }
+    return context.done();
+  }
+}
---
+++ java/greeter/src/main/java/org/apache/flink/statefun/playground/java/greeter/types/Types.java
@@ -0,0 +1,27 @@
+package org.apache.flink.statefun.playground.java.greeter.types;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.statefun.playground.java.greeter.types.generated.UserProfile;
+import org.apache.flink.statefun.sdk.java.TypeName;
+import org.apache.flink.statefun.sdk.java.types.SimpleType;
+import org.apache.flink.statefun.sdk.java.types.Type;
+
+public final class Types {
+
+  private Types() {}
+
+  private static final ObjectMapper JSON_OBJ_MAPPER = new ObjectMapper();
+  private static final String TYPES_NAMESPACE = "greeter.types";
+
+  public static final Type<UserLogin> USER_LOGIN_JSON_TYPE =
+      SimpleType.simpleImmutableTypeFrom(
+          TypeName.typeNameOf(TYPES_NAMESPACE, UserLogin.class.getName()),
+          JSON_OBJ_MAPPER::writeValueAsBytes,
+          bytes -> JSON_OBJ_MAPPER.readValue(bytes, UserLogin.class));
+
+  public static final Type<UserProfile> USER_PROFILE_PROTOBUF_TYPE =
+      SimpleType.simpleImmutableTypeFrom(
+          TypeName.typeNameOf(TYPES_NAMESPACE, UserProfile.getDescriptor().getFullName()),
+          UserProfile::toByteArray,
+          UserProfile::parseFrom);
+}
