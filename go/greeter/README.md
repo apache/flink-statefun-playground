ac4a9c2e350672ce07f43f56a8ef1a41605c4d81
sjwiesman <sjwiesman@gmail.com>
2021-08-24 12:48:46 -0500

    [FLINK-23951] Golang Greeter

---
+++ go/greeter/README.md
@@ -0,0 +1,55 @@
+# The Greeter Example
+
+This is a simple example of a stateful functions application implemented in `Go`.
+
+In this example, we imagine a service that computes personalized greetings. 
+Our service, consist out of the following components:
+
+* `kafka ingress` - This component forwards messages produced to the `names` kafka topic,
+to the `person` stateful function. Messages produced to this topic has the following 
+schema `{ "name" : "bob"}`.
+
+* `person` - This function is triggered by the ingress defined above.
+This function keeps track of the number of visits, and triggers the next functions:
+
+* `greeter` - This function, computes a personalized greeting, based on the name and the number
+of visits of that user. The output of that computation is forward to a Kafka egress defined below.
+ 
+* `kafka egress` - This wraps a Kafka producer that emits `utf-8` greetings to the `greetings` Kafka topic.
+
+
+![Flow](arch.png "Flow")
+
+## Running the example
+
+```
+docker-compose build
+docker-compose up
+```
+
+To observe the customized greeting, as they appear in the `greetings` Kafka topic, run in a separate terminal:
+
+```
+docker-compose exec kafka kafka-console-consumer \
+     --bootstrap-server kafka:9092 \
+     --isolation-level read_committed \
+     --from-beginning \
+     --topic greetings
+```
+
+Try adding few more input lines to [input-example.json](input-example.json), and restart
+the producer service.
+
+```
+docker-compose restart producer
+``` 
+
+Feeling curious? add the following print to the `person` function at [greeter.go](greeter.go):
+```fmt.Printf("Hello there %d!", ctx.Self().Id)```.
+
+Then, rebuild and restart only the `functions` service.
+
+```
+docker-compose build functions
+docker-compose up functions
+```
---
+++ go/greeter/greeter.go
@@ -0,0 +1,123 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"github.com/apache/flink-statefun/statefun-sdk-go/v3/pkg/statefun"
+	"net/http"
+)
+
+type GreetRequest struct {
+	Name   string `json:"name"`
+	Visits int32  `json:"visits"`
+}
+
+var (
+	PersonTypeName      = statefun.TypeNameFrom("example/person")
+	GreeterTypeName     = statefun.TypeNameFrom("example/greeter")
+	KafkaEgressTypeName = statefun.TypeNameFrom("example/greets")
+	GreetRequestType    = statefun.MakeJsonType(statefun.TypeNameFrom("example/GreetRequest"))
+)
+
+type Person struct {
+	Visits statefun.ValueSpec
+}
+
+func (p *Person) Invoke(ctx statefun.Context, message statefun.Message) error {
+	// update the visit count.
+	var visits int32
+	ctx.Storage().Get(p.Visits, &visits)
+
+	visits += 1
+
+	fmt.Printf("seen %d", visits)
+	ctx.Storage().Set(p.Visits, visits)
+
+	// enrich the request with the number of visits.
+	var request GreetRequest
+	if err := message.As(GreetRequestType, &request); err != nil {
+		return fmt.Errorf("failed to deserialize greet reqeuest: %w", err)
+	}
+	request.Visits = visits
+
+	// next, we will forward a message to a special greeter function,
+	// that will compute a personalized greeting based on the number
+	// of visits that this person has been seen.
+	ctx.Send(statefun.MessageBuilder{
+		Target: statefun.Address{
+			FunctionType: GreeterTypeName,
+			Id:           request.Name,
+		},
+		Value:     request,
+		ValueType: GreetRequestType,
+	})
+
+	return nil
+}
+
+func Greeter(ctx statefun.Context, message statefun.Message) error {
+	var request GreetRequest
+	if err := message.As(GreetRequestType, &request); err != nil {
+		return fmt.Errorf("failed to deserialize greet reqeuest: %w", err)
+	}
+
+	greeting := computeGreeting(request.Name, request.Visits)
+
+	ctx.SendEgress(statefun.KafkaEgressBuilder{
+		Target: KafkaEgressTypeName,
+		Topic:  "greetings",
+		Key:    request.Name,
+		Value:  []byte(greeting),
+	})
+
+	return nil
+}
+
+func computeGreeting(name string, visits int32) string {
+	templates := []string{"", "Welcome %s", "Nice to see you again %s", "Third time is the charm %s"}
+	if visits < int32(len(templates)) {
+		return fmt.Sprintf(templates[visits], name)
+	}
+
+	return fmt.Sprintf("Nice to see you for the %d-th time %s!", visits, name)
+}
+
+func main() {
+
+	builder := statefun.StatefulFunctionsBuilder()
+
+	person := &Person{
+		Visits: statefun.ValueSpec{
+			Name:      "visits",
+			ValueType: statefun.Int32Type,
+		},
+	}
+	_ = builder.WithSpec(statefun.StatefulFunctionSpec{
+		FunctionType: PersonTypeName,
+		States:       []statefun.ValueSpec{person.Visits},
+		Function:     person,
+	})
+
+	_ = builder.WithSpec(statefun.StatefulFunctionSpec{
+		FunctionType: GreeterTypeName,
+		Function:     statefun.StatefulFunctionPointer(Greeter),
+	})
+
+	http.Handle("/statefun", builder.AsHandler())
+	_ = http.ListenAndServe(":8000", nil)
+}
