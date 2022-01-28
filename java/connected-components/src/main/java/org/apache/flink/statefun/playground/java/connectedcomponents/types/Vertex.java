dc40776d380540094fa721bfe734c1ca926be9f9
Till Rohrmann <trohrmann@apache.org>
2022-01-28 10:45:10 +0100

    [FLINK-25899] Add connected components example

---
+++ java/connected-components/src/main/java/org/apache/flink/statefun/playground/java/connectedcomponents/types/Vertex.java
@@ -0,0 +1,24 @@
+package org.apache.flink.statefun.playground.java.connectedcomponents.types;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class Vertex {
+
+    @JsonProperty("vertex_id")
+    private int vertexId;
+
+    @JsonProperty("neighbours")
+    private List<Integer> neighbours;
+
+    public Vertex() {}
+
+    public int getVertexId() {
+        return vertexId;
+    }
+
+    public List<Integer> getNeighbours() {
+        return neighbours;
+    }
+}
---
+++ java/connected-components/src/main/java/org/apache/flink/statefun/playground/java/connectedcomponents/types/VertexComponentChange.java
@@ -0,0 +1,41 @@
+package org.apache.flink.statefun.playground.java.connectedcomponents.types;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class VertexComponentChange {
+
+    @JsonProperty("source")
+    private int source;
+
+    @JsonProperty("target")
+    private int target;
+
+    @JsonProperty("component_id")
+    private int componentId;
+
+    public VertexComponentChange() {
+        this(0, 0, 0);
+    }
+
+    private VertexComponentChange(int source, int target, int componentId) {
+        this.source = source;
+        this.target = target;
+        this.componentId = componentId;
+    }
+
+    public int getSource() {
+        return source;
+    }
+
+    public int getTarget() {
+        return target;
+    }
+
+    public int getComponentId() {
+        return componentId;
+    }
+
+    public static VertexComponentChange create(int source, int target, int componentId) {
+        return new VertexComponentChange(source, target, componentId);
+    }
+}
