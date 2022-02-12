b436f8e4d60596f5c00f5be4520f0cf3b87df649
Till Rohrmann <trohrmann@apache.org>
2022-02-12 17:48:17 +0100

    [FLINK-26158] Update java/greeter example to use playground ingress/egress

---
+++ java/greeter/src/main/java/org/apache/flink/statefun/playground/java/greeter/types/EgressRecord.java
@@ -0,0 +1,28 @@
+package org.apache.flink.statefun.playground.java.greeter.types;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class EgressRecord {
+  @JsonProperty("topic")
+  private String topic;
+
+  @JsonProperty("payload")
+  private String payload;
+
+  public EgressRecord() {
+    this(null, null);
+  }
+
+  public EgressRecord(String topic, String payload) {
+    this.topic = topic;
+    this.payload = payload;
+  }
+
+  public String getTopic() {
+    return topic;
+  }
+
+  public String getPayload() {
+    return payload;
+  }
+}
